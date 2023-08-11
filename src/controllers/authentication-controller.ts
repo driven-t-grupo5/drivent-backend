ffc7f44672336f7c12088e0efdacf02ed322442c
vinícius Vieira <viniciuspv.si@gmail.com>
2023-08-11 18:41:41 -0300

    fix: git hublogin

--- src/controllers/authentication-controller.ts
+++ src/controllers/authentication-controller.ts
@@ -1,6 +1,7 @@
 import authenticationService, { SignInParams } from "@/services/authentication-service";
 import { Request, Response } from "express";
 import httpStatus from "http-status";
+
 export async function singInPost(req: Request, res: Response) {
   const { email, password } = req.body as SignInParams;
 
@@ -16,7 +17,7 @@ export async function singInPost(req: Request, res: Response) {
 export async function signInGitHub(req: Request, res: Response) {
   const { code } = req.query;
   try {
-    const result = await authenticationService.signInWithGitHub(code as string);
+    const result =  await authenticationService.signInWithGitHub(code as string);
     return res.status(httpStatus.OK).send(result);
   } catch (error) {
     return res.status(httpStatus.UNAUTHORIZED).send({});  
